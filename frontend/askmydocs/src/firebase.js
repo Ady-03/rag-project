0208df1753ddc758d393ca277d35af7bff6408c5
Ady-03 <kain.adi0304@gmail.com>
2026-05-24 22:41:01 +0530

    api key removed

--- frontend/askmydocs/src/firebase.js
+++ frontend/askmydocs/src/firebase.js
@@ -1,13 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+const apiKey = process.env.GROQ_API_KEY || process.env.REACT_APP_GROQ_API_KEY;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBIbIFIS0NeeJD8Qk9ZIjnGM7i8-gum-JY",
+  apiKey,
   authDomain: "ragchat-0304.firebaseapp.com",
   projectId: "ragchat-0304",
   storageBucket: "ragchat-0304.firebasestorage.app",
   messagingSenderId: "252486495283",
-  appId: "1:252486495283:web:b6f85e03fc0ca758bb4746",
+  appId: "252486495283:web:b6f85e03fc0ca758bb4746",
   measurementId: "G-YPLRS8QMQD",
 };
 
